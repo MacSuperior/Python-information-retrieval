9b40ae95f6ce6a4c2dbebd13d0c66a5fb8ad25d7
Liam Groen <liamgroen@icloud.com>
2023-01-16 22:01:06 +0100

    added func that creates term_indice csv

--- search_engine.py
+++ search_engine.py
@@ -1,9 +1,9 @@
-from ast import arg
 from os import listdir
 import csv
 
 #create a tf_indice for every document in the given folder.
 def calc_term_frequency(folder_path="cran_doc_collection"):
+    global tf_db
     tf_db = {}
     folder_content = listdir(folder_path)
     for file in folder_content:
@@ -17,8 +17,6 @@ def calc_term_frequency(folder_path="cran_doc_collection"):
                     else:
                         tf_db[file].update({word:1})
     return tf_db
-
-print(calc_term_frequency("cran_doc_collection"))
 
 #returns all documents for a single word boolean query
 query = ["appel", "gaar"] #test query
@@ -41,7 +39,27 @@ def results(query: list,incidenceMatrix="recepten_incidence.csv"):
     return vectorList, relDocs    
 
 #TODO: create column for every document and rows for all words in tf_db in a csv file
+def create_csv(file_location="csv_files/term_indice2.csv"):
+    calc_term_frequency()
+    headers = [""]
+    terms = []
+    #create headers list consisting of all document names
+    for doc in tf_db.keys():
+        headers.append(doc)
+
+    #write row to $file_location for every individual word in dataset
+    #TODO it should not seperates every letter with a comma, but it does
+    with open(file_location, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+        for k, v in tf_db.items():
+            v = v.keys()
+            for term in v:
+                terms.append(term)
+            print(k,"\n", v)
+        writer.writerows(terms)
+    return headers, terms
 
 #TODO: fill matrix with 1 if document has occurence of the word, else fill in 0
 
-print(calc_term_frequency())
+print(create_csv())
